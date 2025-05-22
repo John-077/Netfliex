678c32512360cf6fd4c847c4864894ebba8ed0f4
yohanisHs <johnazusa5@gmail.com>
2025-05-22 17:30:11 +0300

    row

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -9,7 +9,7 @@ function Banner() {
     (async () => {
       try {
         const request = await axios.get(requests.fetchNetflixOriginals);
-        console.log(request.data.results);
+        // console.log(request.data.results);
         setMovie(
           request.data.results[
             Math.floor(Math.random() * request.data.results.length)
